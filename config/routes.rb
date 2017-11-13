c58ddd236db5be0384ec784b53bfd9602849c7f3
Graham Cottridge <codinggc@gmail.com>
2017-11-13 16:20:02 +0000

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :bookings do
+    resources :reviews, only: [:show, :edit, :new]
+  end
+end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
