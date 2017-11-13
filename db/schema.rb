8133cf0936ad2e09cd777e42ed958c771ae50b0e
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-13 16:03:39 +0000

    Adding the Bookings Controller, adding view/new, adding routes

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20171113150143) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "testimonial"
+    t.integer  "booking_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 20171113150143) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "reviews", "bookings"
 end
