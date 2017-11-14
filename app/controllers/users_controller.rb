cc85c5d467dd1cdd20cde82d0370ed42e69ec9fd
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-14 23:17:50 +0000

    Implemented facebook connect feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,3 +25,4 @@ class UsersController < ApplicationController
     return user
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
+
+   def self.find_for_facebook_oauth(data)
+      email = data.info.email
+      uid = data.uid
+
+      user = User.where(email: email, uid: uid).first
+
+      if user.nil?
+          User.create({
+              provider: "facebook",
+              uid: uid,
+              email: email,
+              first_name: data.info.first_name,
+            last_name: data.info.last_name,
+            facebook_picture_url: data.info.image,
+            password: Devise.friendly_token[0,20]
+          })
+          # Need to create the user
+      else
+          # return the user
+          user
+      end
+  end
 end
