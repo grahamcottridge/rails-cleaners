6cfaa60d55f64e8d2d29c3b848112adb3c827ed2
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-14 12:51:10 +0000

    Added a booking form and added created booking create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :bookings
+  resources :cleaners do
+    resources :bookings, only: [:new, :create]
+  end
+
+      resources :bookings
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
