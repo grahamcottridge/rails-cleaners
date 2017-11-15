b54f3adb3276b80b525ee76e74ed3e7fbea1ba48
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-15 01:08:30 +0000

    Added some migrations to Users and Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114145750) do
+ActiveRecord::Schema.define(version: 20171115005856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171114145750) do
     t.datetime "updated_at",     null: false
     t.integer  "customer_id"
     t.integer  "cleaner_id"
+    t.text     "message"
+    t.integer  "hrs_work"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -36,20 +38,30 @@ ActiveRecord::Schema.define(version: 20171114145750) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
+    t.integer  "hourly_rate"
+    t.string   "phone_number"
+    t.text     "bio"
+    t.string   "location"
+    t.boolean  "role_cleaner",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
