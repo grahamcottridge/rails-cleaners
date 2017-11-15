eee1baacef3feb07a16c272a21b0a5f65172b0db
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-15 11:57:20 +0000

    added show method and show view to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,24 @@
 class BookingsController < ApplicationController
 #/bookings
 #/cleaners/1/bookings
-  def create
-    @booking = Booking.new(date: booking_params[:date])
-    @booking.customer = current_user
-    @booking.cleaner = User.find(params[:user_id])
-    if @booking.save
-      redirect_to :root
-    else
-      redirect_back(fallback_location: fallback_location)
-    end
-  end
+def show
+  @booking = Booking.find(params[:id])
+end
 
-  private
-
-  def booking_params
-     params.require(:booking).permit(:date)
+def create
+  @booking = Booking.new(date: booking_params[:date])
+  @booking.customer = current_user
+  @booking.cleaner = User.find(params[:user_id])
+  if @booking.save
+    redirect_to :root
+  else
+    redirect_back(fallback_location: fallback_location)
   end
+end
+
+private
+
+def booking_params
+ params.require(:booking).permit(:date)
+end
 end
